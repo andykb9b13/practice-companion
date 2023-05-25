47680ff6896a794b1eae6010c1a2ca1ddaa4cf66
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-24 21:55:45 -0400

    added resolvers for querying all students and assignments as well as adding a student and adding an assignment

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,23 @@
-const resolvers = {};
+const { Student, Assignment } = require("../models");
+
+const resolvers = {
+  Query: {
+    students: async () => {
+      return await Student.find({}).populate("assignments");
+    },
+    assignments: async () => {
+      return await Assignment.find({});
+    },
+  },
+
+  Mutation: {
+    addStudent: async (parent, { name, instrument }) => {
+      return await Student.create({ name, instrument });
+    },
+    addAssignment: async (parent, { name, date, details, userId }) => {
+      return await Assignment.create(name, date, details, userId);
+    },
+  },
+};
 
 module.exports = resolvers;
